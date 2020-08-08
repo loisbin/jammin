122ede337565a2c83eeb6d649f452fa214f784b3
Lois <lois.bin@gmail.com>
2020-08-08 11:13:46 -0700

    Updates

---
+++ precache-manifest.d7940da782ed97830fe726ed5649778f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a05138081a2a3e5501f4b1a78dd027e",
+    "url": "/jammin/index.html"
+  },
+  {
+    "revision": "4cd49b718d4ee8f360c7",
+    "url": "/jammin/static/css/main.c9979e73.chunk.css"
+  },
+  {
+    "revision": "7cfc5608b1cf140ce0bd",
+    "url": "/jammin/static/js/2.70615f32.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/jammin/static/js/2.70615f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd49b718d4ee8f360c7",
+    "url": "/jammin/static/js/main.2068b50e.chunk.js"
+  },
+  {
+    "revision": "8be61f56b1fda36ffd08",
+    "url": "/jammin/static/js/runtime-main.5113d260.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jammin/static/media/logo.5d5d9eef.svg"
+  }
+]);
